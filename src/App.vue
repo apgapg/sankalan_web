19dbb805e406b80f1fdf9d2d9a10e57e32610fa7
Ayush P Gupta <ayushpguptaapg@gmail.com>
2020-01-18 12:46:00 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-app>
+		<v-app-bar
+				app
+				color="white"
+				elevation="2"
+		>
+			<div class="d-flex align-center">
+				<v-img
+						alt="Vuetify Logo"
+						class="shrink mr-2"
+						contain
+						src="./assets/logo.png"
+						transition="scale-transition"
+						width="40"
+				/>
+			</div>
+			<v-toolbar-title>Sankalan: Self Composed Hindi Poems, Stories</v-toolbar-title>
+
+			<v-spacer/>
+			<v-btn
+					href="http://apgapg.github.io/"
+					target="_blank"
+					text
+			>
+				<span class="mr-2 hidden-xs-only">About me</span>
+				<v-icon>mdi-open-in-new</v-icon>
+			</v-btn>
+
+		</v-app-bar>
+
+		<v-content class="pb-8">
+			<router-view/>
+		</v-content>
+		<v-footer class="px-4 py-1" color="teal" dark fixed padless>
+			<div>&copy; {{ new Date().getFullYear() }}</div>
+
+
+			<a class="white--text ml-2" href="https://apgapg.github.io/" target="_blank">Ayush P Gupta</a>
+
+
+			<v-spacer/>
+			<v-btn color="white" href="https://github.com/apgapg" icon small target="_blank" text>
+				<v-icon size="20">mdi-github-circle</v-icon>
+			</v-btn>
+			<v-btn color="white" href="https://www.linkedin.com/in/ayushpgupta/" icon small target="_blank" text>
+				<v-icon size="20">mdi-linkedin</v-icon>
+			</v-btn>
+
+			<v-btn color="white" href="https://www.facebook.com/ayushpgupta" icon small target="_blank" text>
+				<v-icon size="20">mdi-facebook-box</v-icon>
+			</v-btn>
+		</v-footer>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    name: 'App',
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    components: {},
+
+    data: () => ({
+        //
+    }),
+};
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+	<v-container>
+		<v-container :key="i" v-for="(story,i) in stories">
+			<v-card >
+				<v-card-title class="font-weight-bold">{{story.title}}</v-card-title>
+				<v-container class="px-4">
+					<p class="pre-formatted">{{story.content}}</p>
+				</v-container>
+			</v-card>
+		</v-container>
+
+	</v-container>
+</template>
+
+<script>
+import {stories} from "./config/config";
+
+export default {
+    name: "HomePage",
+    data() {
+        return {
+            stories: stories
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+	.pre-formatted {
+		white-space: pre-wrap;
+	}
+</style>
